709f447274426f0a4fb9419379b36094793667dc
rezi4bc <rezydoerasta@gmail.com>
2020-11-23 15:53:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
+import React from 'react'
+// import logo from './logo.svg';
 import './App.css';
+import logoWoy from './123.jpeg'
 
-function App() {
+class HelloMessage extends React.Component {
+
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      umurPT: 12
+    }
+  }
+
+  tick = () => {
+    this.setState({
+      umurPT: this.state.umurPT + 1
+    });
+  }
+
+  thin = () => {
+    this.setState({
+      umurPT: this.state.umurPT - 1
+    });
+  }
+
+  
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>PT SAMID UWUUW</h1>
+        <h2>Didirikan Sejak {this.state.umurPT} tahun yang lalu</h2>
+        <button onClick={() => this.tick()}>nambah umur PT</button>
+        <button onClick={() => this.thin()}>ngurang umut PT</button>
+        {this.state.umurPT === 15 ? (<p>gambar hilang :p</p>) : (<img src={logoWoy} alt="logo" />)} 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +49,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default HelloMessage;
